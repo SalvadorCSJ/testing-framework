15cfd9eef955d62a300d56ef1206f038332abd27
SalvadorCSJ <130864614+SalvadorCSJ@users.noreply.github.com>
2025-05-05 11:37:54 -0300

    3. Classe TestResult

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import tests
+import Test_Framework as TF
 
 def main():
+    
+    result = TF.TestResult()
+
     test = tests.MyTest('test_a')
-    test.run()
+    test.run(result)
 
     test = tests.MyTest('test_b')
-    test.run()
+    test.run(result)
 
     test = tests.MyTest('test_c')
-    test.run()
+    test.run(result)
+
+    print(result.summary())
 
 if __name__ == '__main__':
     main()
