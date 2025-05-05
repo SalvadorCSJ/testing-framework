94936d0ff33fe918c2f164975e1efca382f54a25
SalvadorCSJ <130864614+SalvadorCSJ@users.noreply.github.com>
2025-05-05 11:57:14 -0300

    5. Classe TestSuite

--- main.py
+++ main.py
@@ -4,32 +4,22 @@ import Test_Framework as TF
 def main():
     
     result = TF.TestResult()
+    suite = TF.TestSuite()
 
-    test = tests.TestCaseTest('test_result_success_run')
-    test.run(result)
+    suite.add_test(tests.TestCaseTest('test_result_success_run'))
+    suite.add_test(tests.TestCaseTest('test_result_failure_run'))
+    suite.add_test(tests.TestCaseTest('test_result_error_run'))
+    suite.add_test(tests.TestCaseTest('test_result_multiple_run'))
+    suite.add_test(tests.TestCaseTest('test_was_set_up'))
+    suite.add_test(tests.TestCaseTest('test_was_run'))
+    suite.add_test(tests.TestCaseTest('test_was_tear_down'))
+    suite.add_test(tests.TestCaseTest('test_template_method'))
 
-    test = tests.TestCaseTest('test_result_failure_run')
-    test.run(result)
+    suite.add_test(tests.TestSuiteTest('test_suite_size'))
+    suite.add_test(tests.TestSuiteTest('test_suite_success_run'))
+    suite.add_test(tests.TestSuiteTest('test_suite_multiple_run'))
 
-    test = tests.TestCaseTest('test_result_error_run')
-    test.run(result)
-
-    test = tests.TestCaseTest('test_result_multiple_run')
-    test.run(result)
-
-    test = tests.TestCaseTest('test_was_set_up')
-    test.run(result)
-
-    test = tests.TestCaseTest('test_was_run')
-    test.run(result)
-
-    test = tests.TestCaseTest('test_was_tear_down')
-    test.run(result)
-
-    test = tests.TestCaseTest('test_template_method')
-    test.run(result)
-
-
+    suite.run(result)
     print(result.summary())
 
 if __name__ == '__main__':
