11742c2ec56c2f06df101d7fe810adda869da780
SalvadorCSJ <130864614+SalvadorCSJ@users.noreply.github.com>
2025-05-05 14:41:13 -0300

    7. Executando Todos os Testes

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import Test_Framework as TF
 def main():
     
     loader = TF.TestLoader()
-    suite = loader.make_suite(tests.TestLoaderTest)
+    test_case_suite = loader.make_suite(tests.TestCaseTest)
+    test_suite_suite = loader.make_suite(tests.TestSuiteTest)
+    test_load_suite = loader.make_suite(tests.TestLoaderTest)
+
+    suite = TF.TestSuite()
+    suite.add_test(test_case_suite)
+    suite.add_test(test_suite_suite)
+    suite.add_test(test_load_suite)
 
     runner = TF.TestRunner()
     runner.run(suite)
