dca123d18a4857772467e3f040007a82998300cb
SalvadorCSJ <130864614+SalvadorCSJ@users.noreply.github.com>
2025-05-05 14:39:16 -0300

    6. Classes TestLoader e TestRunner

--- main.py
+++ main.py
@@ -3,24 +3,11 @@ import Test_Framework as TF
 
 def main():
     
-    result = TF.TestResult()
-    suite = TF.TestSuite()
+    loader = TF.TestLoader()
+    suite = loader.make_suite(tests.TestLoaderTest)
 
-    suite.add_test(tests.TestCaseTest('test_result_success_run'))
-    suite.add_test(tests.TestCaseTest('test_result_failure_run'))
-    suite.add_test(tests.TestCaseTest('test_result_error_run'))
-    suite.add_test(tests.TestCaseTest('test_result_multiple_run'))
-    suite.add_test(tests.TestCaseTest('test_was_set_up'))
-    suite.add_test(tests.TestCaseTest('test_was_run'))
-    suite.add_test(tests.TestCaseTest('test_was_tear_down'))
-    suite.add_test(tests.TestCaseTest('test_template_method'))
-
-    suite.add_test(tests.TestSuiteTest('test_suite_size'))
-    suite.add_test(tests.TestSuiteTest('test_suite_success_run'))
-    suite.add_test(tests.TestSuiteTest('test_suite_multiple_run'))
-
-    suite.run(result)
-    print(result.summary())
+    runner = TF.TestRunner()
+    runner.run(suite)
 
 if __name__ == '__main__':
     main()
