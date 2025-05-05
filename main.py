65431dd50fd6d39ca5b8f09a8546cc611202c2bf
SalvadorCSJ <130864614+SalvadorCSJ@users.noreply.github.com>
2025-05-05 11:50:05 -0300

    4. Testando TestCase

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ def main():
     
     result = TF.TestResult()
 
-    test = tests.MyTest('test_a')
+    test = tests.TestCaseTest('test_result_success_run')
     test.run(result)
 
-    test = tests.MyTest('test_b')
+    test = tests.TestCaseTest('test_result_failure_run')
     test.run(result)
 
-    test = tests.MyTest('test_c')
+    test = tests.TestCaseTest('test_result_error_run')
     test.run(result)
+
+    test = tests.TestCaseTest('test_result_multiple_run')
+    test.run(result)
+
+    test = tests.TestCaseTest('test_was_set_up')
+    test.run(result)
+
+    test = tests.TestCaseTest('test_was_run')
+    test.run(result)
+
+    test = tests.TestCaseTest('test_was_tear_down')
+    test.run(result)
+
+    test = tests.TestCaseTest('test_template_method')
+    test.run(result)
+
 
     print(result.summary())
 
